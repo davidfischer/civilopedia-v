0848f89610130c1fdbacdeebfe12c6df07e6fca7
David Fischer <djfische@gmail.com>
2013-08-26 08:16:14 -0700

    Added Android boilerplate

---
+++ src/name/davidfischer/civilopedia/CivilopediaActivity.java
@@ -0,0 +1,15 @@
+package name.davidfischer.civilopedia;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CivilopediaActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
