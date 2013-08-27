57c085f4eb8f8efecd23bb2db62225254bce0905
David Fischer <djfische@gmail.com>
2013-08-26 19:59:50 -0700

    Checkstyle fixes

--- src/name/davidfischer/civilopedia/CivilopediaActivity.java
+++ src/name/davidfischer/civilopedia/CivilopediaActivity.java
@@ -3,12 +3,10 @@ package name.davidfischer.civilopedia;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class CivilopediaActivity extends Activity
-{
+public class CivilopediaActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
