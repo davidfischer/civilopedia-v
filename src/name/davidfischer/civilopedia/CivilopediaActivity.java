eb9b303957fa99786f503b18d205d18741554cd9
David Fischer <djfische@gmail.com>
2013-08-29 07:53:24 -0700

    Added expandable list for selecting civ entry

--- src/name/davidfischer/civilopedia/CivilopediaActivity.java
+++ src/name/davidfischer/civilopedia/CivilopediaActivity.java
@@ -1,29 +1,34 @@
 package name.davidfischer.civilopedia;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.ExpandableListView;
+import android.widget.SimpleExpandableListAdapter;
 
 public class CivilopediaActivity extends Activity {
     private String[] mCategoryTitles;
     private DrawerLayout mDrawerLayout;
-    private ListView mDrawerList;
+    private ExpandableListView mDrawerList;
     private ActionBarDrawerToggle mDrawerToggle;
     private CharSequence mTitle;
 
     private static final String TAG = "Civilopedia";
+    private static final String CATEGORY = "CATEGORY";
+    private static final String CATEGORY_SUBITEM = "CATEGORY_SUBITEM";
 
     /** Called when the activity is first created. */
     @Override
@@ -33,14 +38,29 @@ public class CivilopediaActivity extends Activity {
 
         mCategoryTitles = getResources().getStringArray(
                 R.array.civ_categories_array);
+        new ArrayList<HashMap<String, String>>();
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        mDrawerList = (ExpandableListView) findViewById(R.id.left_drawer);
 
-        // Set the adapter for the list view
-        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
-                R.layout.drawer_list_item, mCategoryTitles));
+        // Set the adapter for the expandable list view
+        // This sets the various category texts into the left panel
+        SimpleExpandableListAdapter expListAdapter =
+                new SimpleExpandableListAdapter(
+                        this,
+                        createGroupList(mCategoryTitles),      // Creating group List.
+                        R.layout.drawer_list_item,             // Group item layout XML.
+                        new String[] { CATEGORY },             // the key of group item.
+                        new int[] { R.id.category_text },      // ID of each group item.-Data under the key goes into this TextView.
+                        createChildList(mCategoryTitles),      // childData describes second-level entries.
+                        R.layout.drawer_sublist_item,          // Layout for sub-level entries(second level).
+                        new String[] { CATEGORY_SUBITEM },     // Keys in childData maps to display.
+                        new int[] { R.id.category_child}       // Data under the keys above go into these TextViews.
+                    );
+        mDrawerList.setAdapter(expListAdapter);
+
         // Set the list's click listener
-        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+        //mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+        mDrawerList.setOnChildClickListener(new DrawerChildClickListener());
 
         // enable ActionBar app icon to behave as action to toggle nav drawer
         getActionBar().setDisplayHomeAsUpEnabled(true);
@@ -67,9 +87,36 @@ public class CivilopediaActivity extends Activity {
         };
         mDrawerLayout.setDrawerListener(mDrawerToggle);
 
+        // If no item is selected (no app state) then open the drawer
         if (savedInstanceState == null) {
-            selectItem(0);
+            setTitle(R.string.app_name);
+            mDrawerLayout.openDrawer(GravityCompat.START);
         }
+    }
+
+    private ArrayList<HashMap<String, String>> createGroupList(String [] categories) {
+        ArrayList<HashMap<String, String>> result = new ArrayList<HashMap<String, String>>();
+        for (int i = 0; i < categories.length; i += 1) {
+            HashMap<String, String> group = new HashMap<String, String>();
+            group.put("CATEGORY", categories[i]);
+            result.add(group);
+        }
+        return result;
+    }
+
+    private ArrayList<ArrayList<HashMap<String, String>>> createChildList(
+            String[] categories) {
+        ArrayList<ArrayList<HashMap<String, String>>> result = new ArrayList<ArrayList<HashMap<String, String>>>();
+        for (int i = 0; i < categories.length; ++i) {
+            ArrayList<HashMap<String, String>> subList = new ArrayList<HashMap<String, String>>();
+            for (int n = 0; n < 3; n++) {
+                HashMap<String, String> child = new HashMap<String, String>();
+                child.put(CATEGORY_SUBITEM, "Sub Item " + n);
+                subList.add(child);
+            }
+            result.add(subList);
+        }
+        return result;
     }
 
     @Override
@@ -82,27 +129,34 @@ public class CivilopediaActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    private class DrawerItemClickListener implements ListView.OnItemClickListener {
-        @Override
+    private class DrawerChildClickListener implements ExpandableListView.OnChildClickListener {
+        /*@Override
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
             selectItem(position);
+        }*/
+
+        @Override
+        public boolean onChildClick(ExpandableListView parent, View v,
+                int groupPosition, int childPosition, long id) {
+            selectItem(groupPosition, childPosition);
+            return true;
         }
     }
 
     //Swaps fragments in the main content view.
-    private void selectItem(int position) {
+    private void selectItem(int groupPosition, int childPosition) {
         // update the main content by replacing fragments
         Fragment fragment = new CivilopediaFragment();
         Bundle args = new Bundle();
-        args.putInt(CivilopediaFragment.ARG_CIVILOPEDIA_CATEGORY, position);
+        args.putInt(CivilopediaFragment.ARG_CIVILOPEDIA_CATEGORY, groupPosition);
         fragment.setArguments(args);
 
         FragmentManager fragmentManager = getFragmentManager();
         fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
 
         // Highlight the selected item, update the title, and close the drawer
-        mDrawerList.setItemChecked(position, true);
-        setTitle(mCategoryTitles[position]);
+        mDrawerList.setSelectedChild(groupPosition, childPosition, true);
+        setTitle(mCategoryTitles[groupPosition]);
         mDrawerLayout.closeDrawer(mDrawerList);
     }
 
