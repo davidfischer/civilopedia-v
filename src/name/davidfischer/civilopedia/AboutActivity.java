e3edd5019f13852488af998c9010d3259e57d24d
David Fischer <djfische@gmail.com>
2013-09-18 08:20:39 -0700

    Added affiliate links and images

--- src/name/davidfischer/civilopedia/AboutActivity.java
+++ src/name/davidfischer/civilopedia/AboutActivity.java
@@ -1,7 +1,10 @@
 package name.davidfischer.civilopedia;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class AboutActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +12,11 @@ public class AboutActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about);
+    }
+
+    public void affiliateLink(View v) {
+        String url = v.getTag().toString();
+        final Intent intent = new Intent(Intent.ACTION_VIEW).setData(Uri.parse(url));
+        startActivity(intent);
     }
 }
